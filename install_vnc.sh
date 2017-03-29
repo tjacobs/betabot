1549b0dff21630469a01293e97636a6348c138ee
Tom Jacobs <tjacobs@expensify.com>
2017-03-29 17:27:25 +1100

    Add install inc

---
+++ install_vnc.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+echo ""
+echo "Open ports 5900 - 5950"
+echo "Connect to port 5901"
+echo ""
+
+sudo apt-get install kubuntu-desktop 
+
+sudo apt-get install tightvncserver
+
+vncserver
+
+
+# DISPLAY=:1
+# startkde
--- sd_restore.sh
+++ sd_restore.sh
@@ -3,8 +3,11 @@
 echo ""
 echo "First, unmount the disk in mac Spotlight -> Disk Utility."
 echo "Then check disk number with ls -l /dev/rdisk*"
+ls -l /dev/rdisk*
 echo ""
 echo "Writing in 10 seconds..."
 sleep 10
 
+echo "Writing... (takes about 10 minutes on a class 10 SD card)"
 gzip -dc ~/Desktop/betabot.gz | sudo dd of=/dev/rdisk2 bs=1m
+echo "Done."
