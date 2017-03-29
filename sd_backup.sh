b23bf1e165522391044331d4dcbca27edd18a57a
Tom Jacobs <tjacobs@expensify.com>
2017-03-29 19:24:32 +1100

    SD backup

--- sd_backup.sh
+++ sd_backup.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
+echo ""
+echo "Reading SD card in 10 seconds, saving to betabot.gz on desktop..."
+sleep 10
+
+echo "Reading SD card and saving to betabot.gz on desktop... (takes about 10 minutes on a class 10 SD card)"
 sudo dd if=/dev/rdisk2 bs=1m | gzip > ~/Desktop/betabot.gz
+echo "Done."
