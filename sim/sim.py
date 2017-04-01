b11c1f257f687fef784b81fe5b69739c8463ac1b
Tom Jacobs <tjacobs@expensify.com>
2017-04-01 18:12:05 +1100

    SIM!

---
+++ sim/sim.py
@@ -0,0 +1,16 @@
+import pybullet as p
+from time import sleep 
+
+#physicsClient = p.connect(p.GUI)
+p.connect(p.SHARED_MEMORY)
+
+planeId = p.loadURDF( "/Users/thomasjacobs/Desktop/betabot/sim/plane.urdf" )
+p.setGravity(0,0,-10)
+
+cubeStartPos = [0,0,1]
+cubeStartOrientation = p.getQuaternionFromEuler([0,0,0])
+boxId = p.loadURDF("/Users/thomasjacobs/Desktop/betabot/betabot_ws/src/betabot_description/urdf/betabot.urdf", cubeStartPos, cubeStartOrientation)
+
+p.setRealTimeSimulation(1)
+
+p.disconnect()
