23684775fc25461ad4ab2cbdfb630a86332d7421
Tom Jacobs <tjacobs@expensify.com>
2017-04-06 15:01:22 +0930

    Simulator moving

--- sim/run_sim.sh
+++ sim/run_sim.sh
@@ -2,5 +2,3 @@
 
 # PyBullet only runs on Python 2 for now
 /usr/bin/python2.7 simulator.py
-
-echo "Done."
--- sim/simulator.py
+++ sim/simulator.py
@@ -2,38 +2,30 @@ import pybullet as p
 import time
 import math
 
-# Constants
+# Simulator constants
 useRealTime = 0
 fixedTimeStep = 0.01
-speed = 10
+speed = 5
 
-# Tunable robot behaviour
-amplitude = 0.8
-jump_amp = 0.5
+# Tunable Betabot behaviour
 maxForce = 3.5
-kneeFrictionForce = 0.00
-kp = 1
-kd = .1
+kp = 1.0
+kd = 0.1
 
 # Motor directions
 motordir=[-1, -1]
-
-# How far can we go
-kneeangle = -2.1834
 
 # Useful for quarter turns
 halfpi = 1.57079632679
 
 # Connect
-#physId = p.connect(p.SHARED_MEMORY)
-#if (physId<0):
 print( "" )
 print( "GPU info:" )
 p.connect(p.GUI)
 
 # Set up physics
 p.setPhysicsEngineParameter(numSolverIterations=50)
-p.setGravity(0,0,0)
+p.setGravity(0, 0, 0)
 p.setTimeStep(fixedTimeStep)
 p.setRealTimeSimulation(0)
 
@@ -54,52 +46,43 @@ for i in range(nJoints):
 right_wheel_joint = jointNameToId['right_wheel_joint']
 left_wheel_joint = jointNameToId['left_wheel_joint']
 
-# 
+# Reset joints
 p.resetJointState(betabot, right_wheel_joint, motordir[0] * halfpi)
 p.resetJointState(betabot, left_wheel_joint, motordir[1] * halfpi)
-#constraintID = p.createConstraint(betabot, right_wheel_joint, betabot, knee_front_leftL_link, p.JOINT_POINT2POINT, [0,0,0], [0,0.005,0.2], [0,0.01,0.2])
-#p.changeConstraint(cid,maxForce=10000)
-p.setJointMotorControl(betabot, right_wheel_joint, p.VELOCITY_CONTROL, 0, kneeFrictionForce)
-p.setJointMotorControl(betabot, left_wheel_joint, p.VELOCITY_CONTROL, 0, kneeFrictionForce)
+p.setJointMotorControl(betabot, right_wheel_joint, p.VELOCITY_CONTROL, 0, 0.0)
+p.setJointMotorControl(betabot, left_wheel_joint, p.VELOCITY_CONTROL, 0, 0.0)
 
 # Gravity go!
 p.setGravity(0, 0, -10)
-
     
-# 
-p.setJointMotorControl2(bodyIndex=betabot, jointIndex=right_wheel_joint, controlMode=p.POSITION_CONTROL, targetPosition=motordir[0] * halfpi, positionGain=kp, velocityGain=kd, force=maxForce)
-p.setJointMotorControl2(bodyIndex=betabot, jointIndex=left_wheel_joint,  controlMode=p.POSITION_CONTROL, targetPosition=motordir[1] * halfpi, positionGain=kp, velocityGain=kd, force=maxForce)
-
-# Real time
+# Real time?
 p.setRealTimeSimulation(useRealTime)
 
-# Save
-#p.saveWorld("betabot_saved.py")
-
-# Log
-#logId = p.startStateLogging(p.STATE_LOGGING_MINITAUR,"betabotLog.txt",[betabot])
-
-# Init move
-t = 0.0
-t_end = t + 100
-i=0
-ref_time = time.time()
-
+# Start
 print( "" )
 print( "Running Betabot simulator. Press 'g' in simulator for full view." )
+t = 0.0
+t_end = t + 10000.0
+reference_time = time.time()
+try:
+	while t < t_end:
+		# Calculate time step
+		if (useRealTime):
+			t = time.time() - reference_time
+		else:
+			t = t + fixedTimeStep
 
-# Move!
-while t < t_end:
-	if (useRealTime):
-		t = time.time() - ref_time
-	else:
-		t = t + fixedTimeStep
+		# Move joints
+		target = math.sin(t * speed) * 300.0;
+		p.setJointMotorControl2(bodyIndex=betabot, jointIndex=right_wheel_joint, controlMode=p.POSITION_CONTROL, targetPosition=motordir[0]*target, positionGain=kp, velocityGain=kd, force=maxForce)
+		p.setJointMotorControl2(bodyIndex=betabot, jointIndex=left_wheel_joint, controlMode=p.POSITION_CONTROL, targetPosition=motordir[1]*target, positionGain=kp, velocityGain=kd, force=maxForce)
 
-	target = math.sin(t * speed) * jump_amp + halfpi;
-	p.setJointMotorControl2(bodyIndex=betabot, jointIndex=right_wheel_joint, controlMode=p.POSITION_CONTROL, targetPosition=motordir[0]*target, positionGain=kp, velocityGain=kd, force=maxForce)
-	p.setJointMotorControl2(bodyIndex=betabot, jointIndex=left_wheel_joint, controlMode=p.POSITION_CONTROL, targetPosition=motordir[1]*target, positionGain=kp, velocityGain=kd, force=maxForce)
+		# Step if not real time
+		if (useRealTime==0):
+			p.stepSimulation()
+			time.sleep(fixedTimeStep)
+except:
+	print( "Exited." )
 
-	if (useRealTime==0):	
-		p.stepSimulation()
-		time.sleep(fixedTimeStep)
-
+# Done
+print( "Done." )
