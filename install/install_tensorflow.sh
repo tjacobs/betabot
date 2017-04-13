1dd0bd2b22429f79ff05ead67cf465ec1937a1ae
Tom Jacobs <hatsmagee@gmail.com>
2017-04-13 10:41:18 +0000

    TF

---
+++ install/install_tensorflow.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# For Python 2.7
+sudo apt-get install python-pip python-dev
+
+wget https://github.com/samjabrahams/tensorflow-on-raspberry-pi/releases/download/v1.0.1/tensorflow-1.0.1-cp27-none-linux_armv7l.whl
+
+sudo pip install tensorflow-1.0.1-cp27-none-linux_armv7l.whl
+
+
