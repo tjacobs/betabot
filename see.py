3a69f22f8181cfdb472cf07a3d225224ce1d28de
Tom Jacobs <hatsmagee@gmail.com>
2017-04-27 12:14:07 +0000

    Vision

--- see.py
+++ see.py
@@ -1,6 +1,7 @@
 import camera
 import time
 import cv2
+import cv_helpers
 
 # Create window
 cv2.namedWindow( "preview" )
@@ -11,12 +12,20 @@ camera.startCamera( (640, 368) )
 
 # Loop
 while True:
+	# Get a frame
 	frame = camera.getFrame()
-	cv2.imshow( "preview", frame )
+	
+	# Process
+	combined_binary, stacked_binary, gradx_binary, grady_binary, mag_binary, dir_binary = cv_helpers.threshold( frame )
+	#thresholded_saturation_image = cv_helpers.hls_select(frame, thresh=(40, 255))
+
+	# Show
+	cv2.imshow( "preview", stacked_binary )
 
 	# Esc key?
 	key = cv2.waitKey(20)
 	if key == 27:
 		break
 
+# Close
 cv2.destroyWindow( "preview" )
